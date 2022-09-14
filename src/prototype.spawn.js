cd8f3acb80b2222fd2615bd097691e2e13233dcf
JustusSager <justus.sager@gmail.com>
2022-09-14 20:14:10 +0200

    bugfix

--- src/prototype.spawn.js
+++ src/prototype.spawn.js
@@ -2,38 +2,42 @@ module.exports = function() {
     StructureSpawn.prototype.createBalancedCreep =
         function(energy, role) {
             var number_of_parts = Math.floor(energy / 200);
-            var body = [];
-            for (let i = 0; i < number_of_parts; i++) {
-                body.push(WORK);
+            if (number_of_parts > 0) {
+                var body = [];
+                for (let i = 0; i < number_of_parts; i++) {
+                    body.push(WORK);
+                }
+                for (let i = 0; i < number_of_parts; i++) {
+                    body.push(CARRY);
+                }
+                for (let i = 0; i < number_of_parts; i++) {
+                    body.push(MOVE);
+                }
+    
+                return this.spawnCreep(body, role + Game.time, { memory: {
+                    role: role,
+                    working: true,
+                    room_home: this.room.name,
+                    room_target: this.room.name
+                }});
             }
-            for (let i = 0; i < number_of_parts; i++) {
-                body.push(CARRY);
-            }
-            for (let i = 0; i < number_of_parts; i++) {
-                body.push(MOVE);
-            }
-
-            return this.spawnCreep(body, role + Game.time, { memory: {
-                role: role,
-                working: true,
-                room_home: this.room.name,
-                room_target: this.room.name
-            }});
         }
     
     StructureSpawn.prototype.createFighterCreep =
         function(energy, role) {
             var number_of_parts = Math.floor(energy / 190);
-            var body = [];
-            for (let i = 0; i < number_of_parts; i++) {
-                body.push(TOUGH);
-            }
-            for (let i = 0; i < number_of_parts; i++) {
-                body.push(ATTACK);
-            }
-            for (let i = 0; i < number_of_parts; i++) {
-                body.push(MOVE);
-                body.push(MOVE);
+            if (number_of_parts > 0) {
+                var body = [];
+                for (let i = 0; i < number_of_parts; i++) {
+                    body.push(TOUGH);
+                }
+                for (let i = 0; i < number_of_parts; i++) {
+                    body.push(ATTACK);
+                }
+                for (let i = 0; i < number_of_parts; i++) {
+                    body.push(MOVE);
+                    body.push(MOVE);
+                }
             }
 
             return this.spawnCreep(body, role + Game.time, { memory: {
@@ -47,20 +51,22 @@ module.exports = function() {
         StructureSpawn.prototype.createCarrierCreep =
         function(energy, role) {
             var number_of_parts = Math.floor((energy - 100) / 100);
-            var body = [];
-            body.push(WORK);
-            for (let i = 0; i < number_of_parts; i++) {
-                body.push(CARRY);
+            if (number_of_parts > 0) {
+                var body = [];
+                body.push(WORK);
+                for (let i = 0; i < number_of_parts; i++) {
+                    body.push(CARRY);
+                }
+                for (let i = 0; i < number_of_parts; i++) {
+                    body.push(MOVE);
+                }
+    
+                return this.spawnCreep(body, role + Game.time, { memory: {
+                    role: role,
+                    working: true,
+                    room_home: this.room.name,
+                    room_target: this.room.name
+                }});
             }
-            for (let i = 0; i < number_of_parts; i++) {
-                body.push(MOVE);
-            }
-
-            return this.spawnCreep(body, role + Game.time, { memory: {
-                role: role,
-                working: true,
-                room_home: this.room.name,
-                room_target: this.room.name
-            }});
         }
 };
