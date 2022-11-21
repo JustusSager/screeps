c4bc1880d966c432db2a177d35c268692149e1b0
JustusSager <justus.sager@gmail.com>
2022-11-21 11:45:58 +0100

    BugFix: To many Work parts

--- src/prototype.spawn.js
+++ src/prototype.spawn.js
@@ -75,7 +75,7 @@ module.exports = function() {
         var number_of_parts = Math.floor((energy - 50) / 100);
         if (number_of_parts > 0) {
             var body = [];
-            for (let i = 0; i < number_of_parts || i < 5; i++) {
+            for (let i = 0; i < number_of_parts && i < 5; i++) {
                 body.push(WORK);
             }
             body.push(MOVE);
