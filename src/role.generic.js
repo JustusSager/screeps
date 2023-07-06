a56043a897f787699a723ee3a587605d9bc07b66
JustusSager <justus.sager@gmail.com>
2023-07-06 22:54:15 +0200

    harvester and transporter as generic tasks

--- src/role.generic.js
+++ src/role.generic.js
@@ -3,8 +3,11 @@ Creep holt sich Energy aus Storage, Container oder Spawn und sucht die nächste 
 Kann über memory.room_target auch zu Targets in anderen Räumen geschickt werden.
 */
 require('prototype.creep')();
+
+var taskHarvester = require('task.harvester');
 var taskBuilder = require('task.builder');
 var taskUpgrader = require('task.upgrader');
+var taskTransporter = require('task.transporter');
 
 module.exports = {
     // a function to run the logic for this role
@@ -17,21 +20,35 @@ module.exports = {
         }
 
         if (creep.memory.task == "searching") {
-            if (creep.room.memory.construction_sites.length > 0) {
-                if (speak) creep.say("builder")
-                creep.memory.task = "builder"
-                creep.memory.target = creep.room.memory.construction_sites[0].id
+            if (creep.store.getFreeCapacity([RESOURCE_ENERGY]) > 0 && !creep.getResourceEnergy()) { // TODO funktioniert nicht
+                creep.memory.task = "harvester";
+                creep.memory.target = "energy_source";
+            } else if (creep.room.memory.amount_dropped_energy > 1000) {
+                if (speak) creep.say("transporter");
+                creep.memory.task = "transporter";
+                creep.memory.target = "transporter";
+            } else if (creep.room.memory.construction_sites.length > 0) {
+                if (speak) creep.say("builder");
+                creep.memory.task = "builder";
+                creep.memory.target = creep.room.memory.construction_sites[0].id;
             } else {
                 if (speak) creep.say("upgrader")
-                creep.memory.task = "upgrader"
-                creep.memory.target = creep.room.controller.id
+                creep.memory.task = "upgrader";
+                creep.memory.target = creep.room.controller.id;
             }
         }
 
-        if (creep.memory.task == "builder") {
-            taskBuilder.run(creep, speak)
-        } else if (creep.memory.task == "upgrader") {
-            taskUpgrader.run(creep, speak)
+        if (creep.memory.task == "harvester") {
+            taskHarvester.run(creep, speak);
+        }
+        else if (creep.memory.task == "transporter") {
+            taskTransporter.run(creep, speak)
+        }
+        else if (creep.memory.task == "builder") {
+            taskBuilder.run(creep, speak);
+        } 
+        else if (creep.memory.task == "upgrader") {
+            taskUpgrader.run(creep, speak);
         }
 
     }
---
+++ src/task.transporter.js
@@ -0,0 +1,97 @@
+module.exports = {
+    run: function(creep, speak) {
+
+        if (creep.room.memory.amount_dropped_energy < 1000) {
+            creep.memory.task = "searching"
+            creep.memory.target = "searching"
+        }
+        
+        // if creep is bringing energy to the spawn but has no energy left
+        if (creep.memory.working == true && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.say('GetEnergy');
+            creep.memory.working = false;
+        }
+        // if creep is harvesting energy but is full
+        else if (creep.memory.working == false && creep.store.getFreeCapacity([RESOURCE_ENERGY]) == 0) {
+            creep.say('StoreEnergy');
+            creep.memory.working = true;
+        }
+
+        // if creep is supposed to transfer energy to the spawn
+        if (creep.memory.working == true) {
+            var target_extension = creep.find_extensions_not_full()[0];
+            if (target_extension) {
+                if(speak){creep.say('Extension');}
+                if (creep.transfer(target_extension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    // move towards the spawn
+                    creep.moveTo(target_extension);
+                }
+                return;
+            }
+            
+            var target_spawn = creep.room.find(FIND_MY_SPAWNS, {filter: (s) => s.energy < s.energyCapacity})[0];
+            if (target_spawn) {
+                if(speak){creep.say('Spawn');}
+                if (creep.transfer(target_spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    // move towards the spawn
+                    creep.moveTo(target_spawn);
+                }
+                return;
+            }
+            
+            var target_tower = creep.find_towers_not_full()[0];
+            if (target_tower) {
+                if(speak){creep.say('Tower');}
+                if (creep.transfer(target_tower, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    // move towards the spawn
+                    creep.moveTo(target_tower);
+                }
+                return;
+            }
+            
+            var target_storage = creep.find_storage_not_full();
+            if (target_storage) {
+                if (speak) {creep.say('Storage');}
+                if (creep.transfer(target_storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    // move towards the spawn
+                    creep.moveTo(target_storage);
+                }
+                return;
+            }
+            
+            if (speak) {creep.say('Controller');}
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+        // if creep is supposed to get energy from target
+        else {
+            var source_ground = creep.find_dropped_rescources()[0];
+            if (source_ground) {
+                if (speak) {creep.say('DroppedItem');}
+                if (creep.pickup(source_ground) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source_ground);
+                }
+                return;
+            }
+            
+            var source_tombstone = creep.find_tombstones()[0];
+            if (source_tombstone != null) {
+                if (speak) {creep.say('Tombstone');}
+                if (creep.withdraw(source_tombstone, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source_tombstone);
+                }
+                return;
+            }
+            
+            var source_container = creep.find_container_not_empty();
+            if (source_container != null) {
+                if (speak) {creep.say('Container');}
+                if (creep.withdraw(source_container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source_container);
+                }
+                return;
+            }
+        }
+    }
+};
