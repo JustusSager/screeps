290aeb0614d9c9a1d2bea0f2d1e3462943370ed0
JustusSager <justus.sager@gmail.com>
2023-07-06 17:07:04 +0200

    static target

--- src/role.builder.js
+++ src/role.builder.js
@@ -9,6 +9,19 @@ var roleUpgrader = require('role.upgrader');
 module.exports = {
     // a function to run the logic for this role
     run: function(creep, speak) {
+        if (!creep.memory.target) {
+            creep.memory.target = "searching"
+        }
+
+        if (creep.memory.target == "searching") {
+            if (creep.room.memory.construction_sites.length > 0) {
+                creep.memory.target = creep.room.memory.construction_sites[0].id
+            }
+        }
+
+        if (!Game.getObjectById(creep.memory.target)) {
+            creep.memory.target = "searching"
+        }
         
         // if creep is bringing energy to the spawn but has no energy left
         if (creep.memory.working == true && creep.store[RESOURCE_ENERGY] == 0) {
@@ -23,16 +36,15 @@ module.exports = {
 
         // if creep is supposed to transfer energy to the spawn
         if (creep.memory.working == true) {
-            var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES); // find closest source
+            // var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES); // find closest source
             if (creep.memory.room_target != undefined && creep.room.name != creep.memory.room_target) {
                 if(speak){creep.say('GoToRoom');}
                 var exit_direction = creep.room.findExitTo(creep.memory.room_target);
                 creep.moveTo(creep.pos.findClosestByPath(exit_direction));
             }
-            else if (constructionSite != undefined) {
-                if(speak) {creep.say('Build');}
-                if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(constructionSite);
+            else if (creep.memory.target != undefined && Game.getObjectById(creep.memory.target)) {
+                if (creep.build(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.getObjectById(creep.memory.target));
                 }
             }
             // try to transfer energy, if the spawn is not in range move towards the source
