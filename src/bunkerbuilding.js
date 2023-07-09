cdd8285a509026f8b7979696d3d4362bf9e7acb0
JustusSager <justus.sager@gmail.com>
2023-07-09 12:54:41 +0200

    new basebuilding

--- src/bunkerbuilding.js
+++ src/bunkerbuilding.js
@@ -1,209 +1,115 @@
-var blueprint = {
-    "spawn": {
-      "pos": [
-        {"x":10,"y":6}
-      ]
-    },
-    "tower": {
-      "pos": [
-        {"x":9,"y":5},
-        {"x":9,"y":6},
-        {"x":9,"y":8},
-        {"x":9,"y":9},
-        {"x":11,"y":7},
-        {"x":7,"y":7}
-      ]
-    },
-    "storage": {
-      "pos": [
-        {"x":8,"y":7}
-      ]
-    },
-    "link": {
-      "pos": [
-        {"x":10,"y":8}
-      ]
-    },
-    "terminal": {
-      "pos": [
-        {"x":10,"y":7}
-      ]
-    },
-    "powerSpawn": {
-      "pos": [
-        {"x":8,"y":8}
-      ]
-    },
-    "nuker": {
-      "pos": [
-        {"x":8,"y":6}
-      ]
-    },
-    "road": {
-      "pos": [
-        {"x":7,"y":6},
-        {"x":8,"y":5},
-        {"x":9,"y":4},
-        {"x":10,"y":5},
-        {"x":11,"y":6},
-        {"x":12,"y":7},
-        {"x":11,"y":8},
-        {"x":10,"y":9},
-        {"x":9,"y":10},
-        {"x":8,"y":9},
-        {"x":7,"y":8},
-        {"x":6,"y":7},
-        {"x":8,"y":3},
-        {"x":7,"y":2},
-        {"x":13,"y":6},
-        {"x":14,"y":5},
-        {"x":10,"y":11},
-        {"x":11,"y":12},
-        {"x":5,"y":8},
-        {"x":4,"y":9},
-        {"x":6,"y":3},
-        {"x":5,"y":4},
-        {"x":4,"y":5},
-        {"x":3,"y":6},
-        {"x":3,"y":7},
-        {"x":3,"y":8},
-        {"x":5,"y":10},
-        {"x":6,"y":11},
-        {"x":7,"y":12},
-        {"x":8,"y":13},
-        {"x":9,"y":13},
-        {"x":10,"y":13},
-        {"x":12,"y":11},
-        {"x":13,"y":10},
-        {"x":14,"y":9},
-        {"x":15,"y":8},
-        {"x":15,"y":7},
-        {"x":15,"y":6},
-        {"x":13,"y":4},
-        {"x":9,"y":1},
-        {"x":8,"y":1},
-        {"x":10,"y":1},
-        {"x":11,"y":2},
-        {"x":12,"y":3}
-      ]
-    },
-    "extension": {
-      "pos": [
-        {"x":6,"y":4},
-        {"x":7,"y":3},
-        {"x":7,"y":5},
-        {"x":5,"y":5},
-        {"x":6,"y":6},
-        {"x":4,"y":6},
-        {"x":5,"y":7},
-        {"x":4,"y":7},
-        {"x":4,"y":8},
-        {"x":6,"y":8},
-        {"x":5,"y":9},
-        {"x":7,"y":9},
-        {"x":6,"y":10},
-        {"x":8,"y":10},
-        {"x":7,"y":11},
-        {"x":9,"y":11},
-        {"x":8,"y":12},
-        {"x":9,"y":12},
-        {"x":10,"y":12},
-        {"x":10,"y":10},
-        {"x":11,"y":11},
-        {"x":11,"y":9},
-        {"x":12,"y":10},
-        {"x":12,"y":8},
-        {"x":13,"y":9},
-        {"x":13,"y":7},
-        {"x":14,"y":8},
-        {"x":14,"y":7},
-        {"x":14,"y":6},
-        {"x":10,"y":4},
-        {"x":11,"y":3},
-        {"x":9,"y":3},
-        {"x":10,"y":2},
-        {"x":9,"y":2},
-        {"x":8,"y":2},
-        {"x":8,"y":4},
-        {"x":12,"y":4},
-        {"x":11,"y":5},
-        {"x":13,"y":5},
-        {"x":12,"y":6}
-      ]
-    },
-    "observer": {
-      "pos": [
-        {"x":11,"y":4}
-      ]
-    },
-    "lab": {
-      "pos": [
-        {"x":12,"y":5},
-        {"x":11,"y":10},
-        {"x":12,"y":9},
-        {"x":13,"y":8},
-        {"x":6,"y":9},
-        {"x":7,"y":10},
-        {"x":8,"y":11},
-        {"x":5,"y":6},
-        {"x":6,"y":5},
-        {"x":7,"y":4}
-      ]
-    },
-    "factory": {
-      "pos": [
-        {"x":10,"y":3}
-      ]
-    }
-  }
-  
-var building_types = {
-    "spawn": STRUCTURE_SPAWN,
-    "tower": STRUCTURE_TOWER,
-    "storage": STRUCTURE_STORAGE,
-    "link": STRUCTURE_LINK,
-    "terminal": STRUCTURE_TERMINAL,
-    "powerSpawn": STRUCTURE_POWER_SPAWN,
-    "nuker": STRUCTURE_NUKER,
-    "road": STRUCTURE_ROAD,
-    "extension": STRUCTURE_EXTENSION,
-    "observer": STRUCTURE_OBSERVER,
-    "lab": STRUCTURE_LAB,
-    "factory": STRUCTURE_FACTORY
-}
-
-var building_rcl = {
-    "spawn": 7,
-    "tower": 3,
-    "storage": 4,
-    "link": 5,
-    "terminal": 6,
-    "powerSpawn": 8,
-    "nuker": 8,
-    "road": 0,
-    "extension": 2,
-    "observer": 8,
-    "lab": 6,
-    "factory": 7
-}
-
-var blueprint_index = [
-  'spawn',
-  'tower',
-  'storage',
-  'link',
-  'terminal',
-  'powerSpawn',
-  'nuker',
-  'road',
-  'extension',
-  'observer',
-  'lab',
-  'factory'
+var blueprint = [
+    {"type": STRUCTURE_LINK, "rcl": 0, "x":10,"y":8},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":7,"y":6},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":8,"y":5},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":9,"y":4},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":10,"y":5},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":11,"y":6},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":12,"y":7},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":11,"y":8},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":10,"y":9},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":9,"y":10},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":8,"y":9},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":7,"y":8},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":6,"y":7},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":8,"y":3},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":7,"y":2},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":13,"y":6},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":14,"y":5},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":10,"y":11},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":11,"y":12},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":5,"y":8},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":4,"y":9},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":6,"y":3},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":5,"y":4},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":4,"y":5},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":3,"y":6},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":3,"y":7},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":3,"y":8},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":5,"y":10},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":6,"y":11},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":7,"y":12},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":8,"y":13},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":9,"y":13},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":10,"y":13},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":12,"y":11},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":13,"y":10},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":14,"y":9},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":15,"y":8},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":15,"y":7},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":15,"y":6},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":13,"y":4},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":9,"y":1},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":8,"y":1},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":10,"y":1},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":11,"y":2},
+    {"type": STRUCTURE_ROAD, "rcl": 0, "x":12,"y":3},
+    {"type": STRUCTURE_SPAWN, "rcl":1, "x":10,"y":6},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":6,"y":4},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":7,"y":3},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":7,"y":5},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":5,"y":5},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":6,"y":6},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":4,"y":6},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":5,"y":7},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":4,"y":7},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":4,"y":8},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":6,"y":8},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":5,"y":9},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":7,"y":9},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":6,"y":10},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":8,"y":10},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":7,"y":11},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":9,"y":11},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":8,"y":12},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":9,"y":12},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":10,"y":12},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":10,"y":10},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":11,"y":11},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":11,"y":9},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":12,"y":10},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":12,"y":8},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":13,"y":9},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":13,"y":7},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":14,"y":8},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":14,"y":7},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":14,"y":6},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":10,"y":4},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":11,"y":3},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":9,"y":3},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":10,"y":2},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":9,"y":2},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":8,"y":2},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":8,"y":4},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":12,"y":4},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":11,"y":5},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":13,"y":5},
+    {"type": STRUCTURE_EXTENSION, "rcl": 2, "x":12,"y":6},
+    {"type": STRUCTURE_TOWER, "rcl": 3, "x":9,"y":6},
+    {"type": STRUCTURE_TOWER, "rcl": 5, "x":9,"y":8},
+    {"type": STRUCTURE_TOWER, "rcl": 7, "x":7,"y":7},
+    {"type": STRUCTURE_TOWER, "rcl": 8, "x":9,"y":5},
+    {"type": STRUCTURE_TOWER, "rcl": 8, "x":9,"y":9},
+    {"type": STRUCTURE_TOWER, "rcl": 8, "x":11,"y":7},
+    {"type": STRUCTURE_STORAGE, "rcl": 4, "x":8,"y":7},
+    {"type": STRUCTURE_TERMINAL, "rcl": 6, "x":10,"y":7},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":12,"y":5},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":11,"y":10},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":12,"y":9},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":13,"y":8},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":6,"y":9},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":7,"y":10},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":8,"y":11},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":5,"y":6},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":6,"y":5},
+    {"type": STRUCTURE_LAB, "rcl": 6, "x":7,"y":4},
+    {"type": STRUCTURE_FACTORY, "rcl": 7, "x":10,"y":3},
+    {"type": STRUCTURE_OBSERVER, "rcl": 8, "x":11,"y":4},
+    {"type": STRUCTURE_POWER_SPAWN, "rcl": 8, "x":8,"y":8},
+    {"type": STRUCTURE_NUKER, "rcl": 8, "x":8,"y":6}
 ]
 
-var counter_inner = 0;
-var counter_outer = 0;
+var counter = 0;
 
 function calculate_offset(dict, flag) {
     return {
@@ -212,57 +118,51 @@ function calculate_offset(dict, flag) {
     }
 }
 
-function place_construction_sites(flag) {
-    if (building_rcl[blueprint[blueprint_index[counter_outer]]] > flag.room.controller.level) {
+function place_construction_sites(flag, rcl_level) {
+    if (counter >= blueprint.length) {
+      counter = 0;
+    }
+    
+    if (blueprint[counter].rcl > rcl_level) {
+      counter++
       return;
     }
-    if (blueprint[blueprint_index[counter_outer]].pos[counter_inner]) {
-        //console.log(JSON.stringify(blueprint[i].pos[counter]));
-        
-        // calculate position
-        let building = calculate_offset(blueprint[blueprint_index[counter_outer]].pos[counter_inner], flag);
-        
-        // check, if a road is blocking a non-road 
-        let check_pos = flag.room.lookAt(building.x, building.y);
-        let road_blocking = false;
-        if (check_pos[0].type == 'structure') {
-          if (check_pos[0].structure.structureType == 'road' && blueprint_index[counter_outer] != 'road') {
-            console.log('Road is blocking non-road');
-            road_blocking = true;
-          }
+
+    // calculate position
+    let building_cords = calculate_offset(blueprint[counter], flag);
+    
+    // check, if a road is blocking a non-road 
+    let check_pos = flag.room.lookAt(building_cords.x, building_cords.y);
+    let road_blocking = false;
+    if (check_pos[0].type == 'structure') {
+      if (check_pos[0].structure.structureType == 'road' && blueprint[counter].type != STRUCTURE_ROAD) {
+        console.log('Road is blocking non-road');
+        road_blocking = true;
+      }
+    }
+
+    console.log('Trying building: ' + JSON.stringify(blueprint[counter]))
+
+    // place construction site
+    let name = flag.room.createConstructionSite(building_cords.x, building_cords.y, blueprint[counter].type);
+
+    // check for errors
+    if (name == ERR_INVALID_TARGET) {
+        console.log('ERR_INVALID_TARGET');
+        if (road_blocking) {
+          // Destroy road if it is blocking a buildable non-road
+          Game.getObjectById(check_pos[0].structure.id).destroy();
+          console.log('Destroy Road at: ' + JSON.stringify(check_pos[0].structure.pos))
         }
-
-        console.log('Trying building: ' + JSON.stringify(building) + ' ' + blueprint_index[counter_outer])
-
-        // place construction site
-        let name = flag.room.createConstructionSite(building.x, building.y, building_types[blueprint_index[counter_outer]]);
-
-        // check for errors
-        if (name == ERR_INVALID_TARGET) {
-            console.log('ERR_INVALID_TARGET');
-            if (road_blocking) {
-              // Destroy road if it is blocking a buildable non-road
-              Game.getObjectById(check_pos[0].structure.id).destroy();
-              console.log('Destroy Road at: ' + JSON.stringify(check_pos[0].structure.pos))
-            }
-        } 
-        else if (name == ERR_INVALID_ARGS) {
-          console.log('ERR_INVALID_ARGS');
-        } 
-        else if (name == ERR_RCL_NOT_ENOUGH) {
-          // Cant build that yet
-          console.log('ERR_RCL_NOT_ENOUGH');
-        }
+    } 
+    else if (name == ERR_INVALID_ARGS) {
+      console.log('ERR_INVALID_ARGS');
+    } 
+    else if (name == ERR_RCL_NOT_ENOUGH) {
+      // Cant build that yet
+      console.log('ERR_RCL_NOT_ENOUGH');
     }
-
-    counter_inner++;
-    if (!blueprint[blueprint_index[counter_outer]].pos[counter_inner]) {
-      counter_inner = 0;
-      counter_outer++;
-    }
-    if (!blueprint[blueprint_index[counter_outer]]) {
-      counter_outer = 0;
-    }
+    counter++;
 }
 
 module.exports = {
@@ -272,8 +172,9 @@ module.exports = {
         }
         let flags = [Game.flags.BunkerFlag]
         for (let i in flags) {
+          let rcl_level = flags[i].room.controller.level
           if(flags[i].room.memory.num_construction_sites < 10) {
-            place_construction_sites(flags[i]);
+            place_construction_sites(flags[i], rcl_level);
             flags[i].room.memory.num_construction_sites = flags[i].room.find(FIND_CONSTRUCTION_SITES).length;
           }
         }
